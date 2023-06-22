5de01ea55dababc4935c92767e5ea27b81828b30
Anmol Gupta <anmolgupta081202@gmail.com>
2023-06-23 01:20:48 +0530

    Final

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const env = require('./environment');
 
-// mongoose.connect('mongodb+srv://anmolgupta081202:anmol08@cluster0.hbygl3s.mongodb.net/talkoholic');
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(`mongodb://localhost/${env.db}`);
 
 const db = mongoose.connection;
 db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
