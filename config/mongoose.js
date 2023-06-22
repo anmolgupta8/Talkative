243356308eab4b946b9d81c3a29e5cf935c48ae7
Anmol Gupta <anmolgupta081202@gmail.com>
2023-06-23 00:26:49 +0530

    Deployment Updates

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const env = require('./environment');
 
-mongoose.connect(`mongodb://localhost/${env.db}`);
+mongoose.connect('mongodb+srv://anmolgupta081202:anmol08@cluster0.hbygl3s.mongodb.net/talkoholic');
 
 const db = mongoose.connection;
 db.on('error',console.error.bind(console,"Error connecting to MongoDB"));
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const env = require('./config/environment');
+require('dotenv');
 const app = express();
 require('./config/view-helpers')(app);
-const port = 8000;
+const port = process.env.PORT|| 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
 const cookieParser = require('cookie-parser');
